2c395c6b46bc5b36771502e0775addfa73ba209f
Sean.Rennie <sean.rennie6@gmail.com>
2018-12-03 18:16:29 +0000

    updated all the "find me on" links

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  extends: ['airbnb-base', 'prettier', 'prettier/react'],
+  plugins: ['prettier', 'jsx-a11y', 'import'],
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'object-curly-newline': 0,
+    'arrow-parens': 0,
+    'no-underscore-dangle': 0
+  }
+};
